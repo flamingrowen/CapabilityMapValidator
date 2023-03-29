a68322fe642eac01d871ec233e652ea21e550662
Rohith Anil Kumar <roanilkw@amazon.com>
2023-03-30 04:10:59 +0530

    Updating the Capability Map Validator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,12 @@
 package org.example;
 
 import java.io.*;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.LinkedHashSet;
@@ -24,6 +23,7 @@ public class Main {
     }
     public static void main(String[] args) {
         JSONParser jsonParser = new JSONParser();
+        String path = "src/main/resources/withoutchoicestate.json";
         //can put filepath for needed json file when an instance/object of this class is made; for now we are using this json file for testing.
         try (FileReader reader = new FileReader("src/main/resources/withoutchoicestate.json"))
         {
@@ -34,22 +34,28 @@ public class Main {
             boolean checkDuplicateStatesResult = false;
             boolean validateCapabilityMapStatesResult = false;
             boolean validateTransitionFromStatesResult = false;
+            //check if the basic capability map syntax is valid or not
             if(validationCapabilityMapSyntax == true) {
                 JSONObject states = (JSONObject) jsonObject.get("States");
                 String startAt = (String) jsonObject.get("StartAt");
-                checkDuplicateStatesResult = checkDuplicateStates(states);
-                validateCapabilityMapStatesResult = validateCapabilityMapStates(states);
-                validateTransitionFromStatesResult = validateTransitionFromStates(states,startAt);
+                String jsonString = new String(Files.readAllBytes(Paths.get(path)));
+                // check if there are duplicate states in the object field "States"
+                checkDuplicateStatesResult = checkDuplicateStates(states,jsonString);
+                if(checkDuplicateStatesResult == true)
+                {
+                    //validate the transition from start state till the end
+                    validateTransitionFromStatesResult = validateTransitionFromStates(states, startAt);
+                }
             }
             else
                 System.out.println("Invalid Capability Map Syntax");
-            if(checkDuplicateStatesResult == true && validateCapabilityMapStatesResult == true && validateTransitionFromStatesResult == true)
+            if(checkDuplicateStatesResult == true && validateTransitionFromStatesResult == true && validationCapabilityMapSyntax == true)
             {
-                System.out.println("Valid");
+                System.out.println("Valid Capability Map");
             }
             else
             {
-                System.out.println("Not Valid");
+                System.out.println("Invalid Capability Map");
             }
         }
         catch (FileNotFoundException e) {
@@ -96,19 +102,59 @@ public class Main {
     }
 
     //check for Duplicate States in the object field named "States"
-    static boolean checkDuplicateStates(JSONObject states)
+    static boolean checkDuplicateStates(JSONObject states, String jsonString)
     {
-
-
-        return true;
+        boolean checkDuplicateStatesResult = true;
+        Set<String> keys = states.keySet();
+        String keysStringArray[] = keys.toArray(new String[keys.size()]);
+        //System.out.println("Keys: "+keys);
+        //System.out.println("Length of set of keys in object: "+keys.size());
+        String key = "";
+        int startIndex,firstIndex;
+        int counter;
+        //System.out.println(jsonString);
+        for(int i=0;i<keysStringArray.length;i++)
+        {
+            key = keysStringArray[i];
+            startIndex = 0;
+            firstIndex = 0;
+            counter = 0;
+            //System.out.println("Key: "+key);
+            while(startIndex >= 0 && startIndex <= jsonString.length()) {
+                firstIndex = jsonString.indexOf(key,startIndex);
+                //System.out.println("First Index of word: "+firstIndex);
+                if(firstIndex == -1)
+                    break;
+                startIndex = firstIndex + key.length();
+                //System.out.println("New start index: "+startIndex);
+                ++counter;
+            }
+            //System.out.println("Counter for key " + key +": "+counter);
+            if(counter > 2)
+            {
+                System.out.println("Duplicate keys found");
+                checkDuplicateStatesResult = false;
+                break;
+            }
+        }
+        System.out.println("checkDuplicateStatesResult: " + checkDuplicateStatesResult);
+        return checkDuplicateStatesResult;
     }
 
     //validate the fields of each state
-    static boolean validateCapabilityMapStates(JSONObject states)
+    static boolean validateCapabilityMapStates(JSONObject currentState, String type)
     {
-        System.out.println("validateCapabilityMapStates Test");
-
-        return true;
+        boolean validateCapabilityMapStatesResult = false;
+        //System.out.println("validateCapabilityMapStates Test");
+        if(type.equals("Task") == true)
+        {
+            validateCapabilityMapStatesResult = validateTaskState(currentState);
+        }
+        if(type.equals("Choice") == true)
+        {
+            validateCapabilityMapStatesResult = validateChoiceState(currentState);
+        }
+        return validateCapabilityMapStatesResult;
     }
 
     //take the firstState and transition till the end
@@ -117,18 +163,22 @@ public class Main {
         boolean validateResult = false;
         boolean nextStateValid = true;
         boolean typeValidateResult = false;
+        boolean validateCapabilityMapStatesResult = false;
         String type = "";
         boolean end = false;
+        //Succeed state and Fail state (Terminal State)
+        //boolean succeed = false;
+        //boolean fail = false;
         String statePointer= startAt;
         JSONObject currentState = null;
         JSONObject nextState = null;
-        System.out.println("States: "+states);
+        //System.out.println("States: "+states);
         while(nextStateValid == true)
         {
 
-            System.out.println("State pointer: "+statePointer);
+            //System.out.println("State pointer: "+statePointer);
             currentState = (JSONObject) states.get(statePointer);
-            System.out.println("Current state: "+currentState);
+            //System.out.println("Current state: "+currentState);
             type = (String) currentState.get("Type");
             try {
                 end = (boolean) currentState.get("End");
@@ -140,8 +190,10 @@ public class Main {
             if(type != null)
             {
                 typeValidateResult = validateStateType(type);
-                System.out.println("typeValidateResult: "+typeValidateResult);
-                System.out.println("End: "+end);
+                //System.out.println("typeValidateResult: "+typeValidateResult);
+                //validate the structure of a particular state
+                validateCapabilityMapStatesResult = validateCapabilityMapStates(currentState,type);
+                //System.out.println("End: "+end);
                 if(typeValidateResult == true)
                 {
                     if(end == true)
@@ -153,7 +205,7 @@ public class Main {
                     {
                         if(type.equals("Choice") == false) {
                             statePointer = (String) currentState.get("Next");
-                            System.out.println("Next state pointer: "+statePointer);
+                            //System.out.println("Next state pointer: "+statePointer);
                             if (statePointer == null) {
                                 validateResult = false;
                                 break;
@@ -164,8 +216,8 @@ public class Main {
                                 else {
                                     nextStateValid = false;
                                     validateResult = false;
-                                    System.out.println("Invalid Transition");
-                                    System.out.println("No " + statePointer + " found.");
+                                    //System.out.println("Invalid Transition");
+                                    //System.out.println("No " + statePointer + " found.");
                                 }
                             }
                         }
@@ -184,7 +236,7 @@ public class Main {
             }
 
         }
-        System.out.println(states);
+        //System.out.println(states);
         //System.out.println("validateTransitionFromStates Test");
 
         return validateResult;
@@ -210,5 +262,56 @@ public class Main {
         if (types.contains(s) == true)
             validateStateTypeResult = true;
         return validateStateTypeResult;
+    }
+
+    //check if the
+    static boolean validateChoiceState(JSONObject choiceState)
+    {
+        boolean validateChoiceStateResult = true;
+
+
+        return validateChoiceStateResult;
+    }
+
+    //check if the Resource field is present or not (MANDATORY)
+    //check if the TimeoutSeconds and HeartbeatSeconds fields are positive integers
+    //check if the HeartbeatSeconds interval is smaller than the TimeoutSeconds value
+    //A Task State may have "TimeoutSecondsPath" and "HeartbeatSecondsPath" fields (PENDING - VALIDATION NOT DONE)
+    //A Task State MAY include a "Credentials" field, whose value MUST be a JSON object whose value is defined by the interpreter. (PENDING - VALIDATION NOT DONE)
+    static boolean validateTaskState(JSONObject taskState)
+    {
+        boolean validateTaskStateResult = false;
+        boolean validateTimeoutSeconds = true;
+        boolean validateHeartbeatSeconds = true;
+        System.out.println("Task State: "+taskState);
+        String resource = (String) taskState.get("Resource");
+        long timeoutSeconds = 0;
+        long heartbeatSeconds = 0;
+        try
+        {
+            timeoutSeconds = (long) taskState.get("TimeoutSeconds");
+            heartbeatSeconds = (long) taskState.get("HeartbeatSeconds");
+            if((heartbeatSeconds >= timeoutSeconds))
+            {
+                validateTimeoutSeconds = false;
+                validateHeartbeatSeconds = false;
+
+            }
+            if(heartbeatSeconds <= 0 || timeoutSeconds <= 0)
+            {
+                validateTimeoutSeconds = false;
+                validateHeartbeatSeconds = false;
+            }
+        }
+        catch(NullPointerException e)
+        {
+            validateTimeoutSeconds = true;
+            validateHeartbeatSeconds = true;
+        }
+
+        if(resource != null && validateTimeoutSeconds == true && validateHeartbeatSeconds == true)
+            validateTaskStateResult = true;
+        //System.out.println("validateTaskStateResult: "+validateTaskStateResult);
+        return validateTaskStateResult;
     }
 }
